24a72adb612f6127540ce81a87a3c53705869667
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-08-05 16:23:20 +0200

    Add files via upload

--- project/client.py
+++ project/client.py
@@ -1,20 +1,25 @@
 import requests
 
 BASE = 'http://127.0.0.1:5000/'
+resource_owner = 'owner/'
 resource = 'item/'
-resource_item = 'chair3'
+resource_item = 'chair'
 resource_item2 = 'tablet'
 
 def handle_reuqest():
-    response = requests.post(BASE + resource + resource_item, params={'http':'','https':'','price':12.00})
+
+    response = requests.post(BASE + resource_owner + 'Artur', params={'http':'','https':''})
+    print(response.json())
+    response = requests.post(BASE + resource + resource_item, params={'http':'','https':'','price':12.00, 'owner_id':1})
     response = requests.get(BASE + resource + resource_item, params={'http':'','https':''})
     print(response.json())
-    response = requests.post(BASE + resource + resource_item2, params={'http':'','https':'','price':120.00})
+    response = requests.post(BASE + resource + resource_item2, params={'http':'','https':'','price':120.00, 'owner_id':1})
     response = requests.get(BASE + resource + resource_item2, params={'http':'','https':''})
+    print(response.json())
+    response = requests.get(BASE + resource_owner + 'Artur', params={'http':'','https':''})
     print(response.json())
 
 def main():
-
     handle_reuqest()
 
 if __name__ == '__main__':
